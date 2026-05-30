7dfa3ea2c3bb04f0f715d4c190d4285b498493b8
Crangini <164672343+Crangini@users.noreply.github.com>
2026-05-30 23:24:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -169,12 +169,9 @@ client.on("guildMemberAdd", async (member) => {
   }
 
   if (!welcomeChannel) {
-    logger.warn({ guildId: guild.id }, "Salon de bienvenue introuvable");
+} else {
     return;
   }
-
-  try {
-    await member.roles.add("1510238919445450772");
     logger.info({ guildId: guild.id, userId: member.id }, "Rôle automatique attribué");
   } catch (err) {
     logger.error({ err, guildId: guild.id }, "Erreur lors de l'attribution du rôle automatique");
