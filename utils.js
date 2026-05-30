403dc912d8de54bef728224aa25ac16e97704728
Crangini <164672343+Crangini@users.noreply.github.com>
2026-05-31 00:20:05 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,16 +1,13 @@
-export const GIVEAWAY_ROLES = [
+export const MOD_ROLES = [
   "1510238686959108177",
-  "1510238688674713753",
   "1510238688133644371",
+  "1510238688674713753",
+  "1510238690692173824",
   "1510238684790915214",
 ];
 
-export const MOD_ROLES = [
-  ...GIVEAWAY_ROLES,
-  "1510238690692173824",
-];
+export const GIVEAWAY_ROLES = MOD_ROLES;
 
 export function hasRole(member, allowedRoles) {
   return allowedRoles.some((roleId) => member.roles.cache.has(roleId));
 }
-
